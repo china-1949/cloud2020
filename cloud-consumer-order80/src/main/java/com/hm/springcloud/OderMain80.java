11eb1ad692b7f1e52b26974de924fc5d04f02dbe
“chenseng123 <your_regist1483202142@qq.com>
2020-05-05 14:41:51 +0800

    Ribbon核心组件IRule

--- cloud-consumer-order80/src/main/java/com/hm/springcloud/OderMain80.java
+++ cloud-consumer-order80/src/main/java/com/hm/springcloud/OderMain80.java
@@ -1,11 +1,14 @@
 package com.hm.springcloud;
 
+import com.hm.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient  //客户端消费者
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OderMain80.class,args);
