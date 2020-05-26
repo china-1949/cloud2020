fb0984cb670d1f587eec01776f5fc97b659c38c5
“chenseng123 <your_regist1483202142@qq.com>
2020-05-26 18:24:40 +0800

    GateWay常用的路由配置Route Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);  //2020-05-26T17:05:11.744+08:00[Asia/Shanghai]
+    }
+}
