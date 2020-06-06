abf326223f4201b677717b5fdc1c7d668a7b3f98
“chenseng123 <your_regist1483202142@qq.com>
2020-06-07 00:22:13 +0800

    Config客户端配置与测试,Config动态刷新之手动版

---
+++ cloud-config-center-3355/src/main/java/com/hm/springcloud/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.hm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+    /**
+     * 需要运维人员发送Post请求刷新3355
+     * curl  -X POST "http://localhost:3355/actuator/refresh"
+     */
+    @Value("${config.info}")  //yml中config
+    private String configInfo;
+
+    @RequestMapping("/configInfo")
+    public String getServerPort() {
+        return configInfo;
+    }
+}
