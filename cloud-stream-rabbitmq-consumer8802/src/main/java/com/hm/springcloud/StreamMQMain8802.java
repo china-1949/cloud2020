f8b9f03bf5b32de23c275952f2f7f38a6fe60ef3
“chenseng123 <your_regist1483202142@qq.com>
2020-06-11 00:14:18 +0800

    98_Nacos之服务提供者注册

--- cloud-stream-rabbitmq-consumer8802/src/main/java/com/hm/springcloud/StreamMQMain8802.java
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/hm/springcloud/StreamMQMain8802.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud;
+package com.hm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
